0e9d5749b895a67b442714bb9e2f491ba397091d
nelioamaral <nelio.amaral@hotmail.com>
2022-05-11 23:13:17 -0300

    First Steps

---
+++ Desafio 2.py
@@ -0,0 +1,4 @@
+dia = input ( ' Dia (numero) = ')
+mes = input ( ' Mês (escrito) = ' )
+ano = input ( ' Ano (numero) = ' )
+print ( ' Você nasceu no dia ' + dia + ' de ' + mes + ' de ' , ano ,'.' + ' Correto? ')
---
+++ Desafio 3.py
@@ -0,0 +1,4 @@
+n1 = float (input(' Digite valor 1: '))
+n2 = float (input(' Digite valor 2: '))
+s= n1+n2
+print(f'O resultado de {n1} mais {n2} é {s}')
